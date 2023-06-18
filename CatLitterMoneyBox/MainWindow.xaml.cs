bb2efb025c8de5182890bfc4a9bd5786de8fea41
Geoffrey-Fish <earl@geoffrey.fish>
2023-06-18 11:53:50 +0200

    CatLitterBox

--- CatLitterMoneyBox/MainWindow.xaml.cs
+++ CatLitterMoneyBox/MainWindow.xaml.cs
@@ -1,211 +1,272 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Windows;
 
 using Newtonsoft.Json;
 
 namespace CatLitterMoneyBox;
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
+
 public partial class MainWindow: Window
     {
     #region statics
+
     public MainWindow()
         {
         InitializeComponent();
+        DataContext = this; //This is a binder that says the mainwindow itself is source for all interface elements
+        User_drpdwn.ItemsSource =
+            BankAccounts; //This is the connection between the dropdown menu and my List!it is that easy!!!
+        InitializeBankAccounts();
+        Closing += MainWindow_Closing;
         }
 
     //Some static values so my code works per se
     public static bool CheckOne; //Checkbox checked
     public static bool CheckTwo; //Checkbox checked
-    public static bool PauliDidIt; //Radiobutton checked
-    public static bool FeliDidIt; //Radiobutton checked
-    public static double BufferSum; //unneccesary for calculation, only for my brain
+    public static bool CheckLottery; //Checkbox checked
+    public static bool UserCreation; // New user button pressed
+    public static bool UserDeletion; // User deletion button pressed
+    public static bool SonderZahlung; // Extra money,yeah
+    public static bool LohnAnpassung; //We want more!We want more!
+    private static List<BankAccount> BankAccounts; //Placeholder list for the database
     public static DateTime Today;
-    public static string pathF = "feliBank.json";
-    public static string pathP = "pauliBank.json";
-    #endregion
+    private static readonly string path = ".\\BankAccounts.json"; //Path to database
+
+    #endregion statics
 
     #region Checkboxes & Radios
+
     private void Kloeins_cb_Checked(object sender, RoutedEventArgs e)
         {
         CheckOne = CheckOne ? false : true;
         }
+
     private void Klozwei_cb_Checked(object sender, RoutedEventArgs e)
         {
         CheckTwo = CheckTwo ? false : true;
         }
-    private void Feli_radbt_Checked(object sender, RoutedEventArgs e)
+
+    private void BonusLottery_cb_Checked(object sender, RoutedEventArgs e)
         {
-        FeliDidIt = FeliDidIt ? false : true;
-        PauliDidIt = FeliDidIt ? false : true;
+        CheckLottery = CheckLottery ? false : true;
         }
-    private void Pauli_radbt_Checked(object sender, RoutedEventArgs e)
-        {
-        PauliDidIt = PauliDidIt ? false : true;
-        FeliDidIt = PauliDidIt ? false : true;
-        }
-    #endregion
+
+    #endregion Checkboxes & Radios
+
     #region Bankaccounts
 
-    //Get Account data and serve it.If never run before, generate it
-    //With Newtons Json converted into bits
-    public static BankAccount GetFeliAccount()
+    private void InitializeBankAccounts() //Load and initialize the database list of Bankaccounts
         {
-        if(File.Exists(pathF))
-            {
-            BankAccount fe = JsonConvert.DeserializeObject<BankAccount>(File.ReadAllText(pathF));
-            return fe;
-            }
-        else
-            {
-            return new BankAccount("Feli", DateTime.Now, 0.00);
-            }
+        if(File.Exists(path))
+            BankAccounts = JsonConvert.DeserializeObject<List<BankAccount>>(File.ReadAllText(path));
         }
-    public static BankAccount GetPauliAccount()
-        {
-        if(File.Exists(pathP))
-            {
-            BankAccount pa = JsonConvert.DeserializeObject<BankAccount>(File.ReadAllText(pathP));
-            return pa;
-            }
-        else
-            {
-            return new BankAccount("Pauli", DateTime.Now, 0.0);
-            }
-        }
-    #endregion
 
+    public BankAccount SelectedAccount { get; set; } //getter from the dropdown menu
+
+    private void UserAnlegen_btn_Click(object sender, RoutedEventArgs e) //New User creation
+        {
+        //visibility on
+        UserCreationName_tbx.Visibility = 0;
+        UserCreationName_lbl.Visibility = 0;
+        UserManipulation_btn.Visibility = 0;
+        UserVorgang_lbl.Content = "Erstellmodus";
+        //Set flags for submit button
+        UserCreation = true;
+        UserDeletion = false;
+        }
+
+    private void UserLöschen_btn_Click(object sender, RoutedEventArgs e) //Userdeletion
+        {
+        //visibility on
+        UserCreationName_tbx.Visibility = 0;
+        UserCreationName_lbl.Visibility = 0;
+        UserManipulation_btn.Visibility = 0;
+        UserVorgang_lbl.Content = "Löschvorgang";
+        //Set flags for submit button
+        UserDeletion = true;
+        UserCreation = false;
+        }
+
+    private void UserManipulation_btn_Click(object sender, RoutedEventArgs e) //process chosen order
+        {
+        if(UserCreation)
+            {
+            var userName = UserCreationName_tbx.Text;
+            BankAccounts.Add(new BankAccount(userName, DateTime.Today, 0.10, 0));
+            }
+
+        if(UserDeletion)
+            {
+            UserCreationName_tbx.Focusable = false; //just read, dont touch
+            UserCreationName_tbx.Text = SelectedAccount.Name; //Show name for ref
+            BankAccounts.Remove(SelectedAccount);
+            }
+
+        //Visibility off
+        UserCreationName_tbx.Visibility = (Visibility)1;
+        UserCreationName_lbl.Visibility = (Visibility)1;
+        UserManipulation_btn.Visibility = (Visibility)1;
+        //Clear to status quo
+        UserVorgang_lbl.Content = "";
+        UserCreation = false;
+        UserDeletion = false;
+        }
+
+    #endregion BankAccounts
+
+    #region Money special buttons
+
+    //Change the amount given for the job done
+    private void LohnAnpassen_btn_Click(object sender, RoutedEventArgs e)
+        {
+        ValueChange_tbx.Focus();
+        ValueChange_tbx.Text = SelectedAccount.Salary.ToString("C");
+        //Flags for submit button
+        LohnAnpassung = true;
+        SonderZahlung = false;
+        }
 
     //Adding Money
-    private void Berechnen_btn_Click(object sender, RoutedEventArgs e)
+    private void SonderEinzahlung_btn_Click(object sender, RoutedEventArgs e)
         {
+        ValueChange_tbx.Focus();
+        //Flags for submit button
+        SonderZahlung = true;
+        LohnAnpassung = false;
+        }
+
+    private void ValueChange_btn_Click(object sender, RoutedEventArgs e)
+        {
+        if(LohnAnpassung)
+            {
+            double.TryParse(ValueChange_tbx.Text, out var result);
+            SelectedAccount.Salary = result;
+            }
+
+        if(SonderZahlung)
+            {
+            double.TryParse(ValueChange_tbx.Text, out var result);
+            SelectedAccount.Money += result;
+            }
+
+        //set Flags back to negative
+        LohnAnpassung = false;
+        SonderZahlung = false;
+        }
+
+    #endregion Money special buttons
+
+    #region Money manipulations
+
+    //Little fun lottery for extraordinary jobs done.Gets added
+    private static double Lottery()
+        {
+        var wins = new[] { 0.5, 0.10, 0.5, 0.15, 0.10, 0.20, 0.15, 0.30 };
+        var rand = new Random();
+        return wins[rand.Next(8)];
+        }
+
+    private void JobBuchen_btn_Click(object sender, RoutedEventArgs e)
+        {
+        var salary = SelectedAccount.Salary;
         Today = Calendar_cal.SelectedDate.Value;
         double i = 0;
         if(CheckOne)
-            i += 0.10;
+            i += salary;
         if(CheckTwo)
-            i += 0.10;
-        BufferSum += i;
-        BankAccount bankbuffer;
-        if(FeliDidIt)
+            i += salary;
+        if(CheckLottery)
             {
-            bankbuffer = GetFeliAccount();
-            bankbuffer.Money += BufferSum;
-            bankbuffer.Date = Today;
-            File.WriteAllText(pathF, JsonConvert.SerializeObject(bankbuffer));
+            var lottery = Lottery();
+            i += lottery;
+            MessageBox.Show($"{lottery.ToString("C")} Euro gewonnen!!");
             }
-        if(PauliDidIt)
-            {
-            bankbuffer = GetPauliAccount();
-            bankbuffer.Money += BufferSum;
-            bankbuffer.Date = Today;
-            File.WriteAllText(pathP, JsonConvert.SerializeObject(bankbuffer));
-            }
+
+        SelectedAccount.Money += i;
+        SelectedAccount.Date = Today;
         MessageBox.Show("Geld erfolgreich eingezahlt!");
         }
 
-
-    //Ask for current balance of all accounts.
-    //Could be changed to only show account of
-    //radiobutton selected user.
     private void AbfrageGuthaben_btn_Click(object sender, RoutedEventArgs e)
         {
-        BankAccount pupil = FeliDidIt ? GetFeliAccount() : GetPauliAccount();
-
-        MessageBox.Show($"{pupil.Name} hat: {pupil.Money.ToString("0.00")} Euro. Stand: {pupil.Date.ToShortDateString()}.\n");
-        pupil = null;
+        MessageBox.Show(
+            $"{SelectedAccount.Name} hat: {SelectedAccount.Money.ToString("0.00")} Euro. Stand: {SelectedAccount.Date.ToShortDateString()}.\n");
         }
 
+    #endregion Money manipulations
+
     #region Abbuchungsvorgang
+
     //Inputbox only active if button pressed. For security lol
     private void AbhebenGuthaben_btn_Click(object sender, RoutedEventArgs e)
         {
-        Abhebebox_tb.IsEnabled = true;
+        Abhebebox_tbx.IsEnabled = true;
         }
+
     //Moneywithdrawal if sufficient balance on account
-    private void Buchen_btn_Click(object sender, RoutedEventArgs e)
+    private void Abheben_btn_Click(object sender, RoutedEventArgs e)
         {
         Today = Calendar_cal.SelectedDate.Value;
-        BankAccount bufferAccount = FeliDidIt ? GetFeliAccount() : GetPauliAccount();
-        string path = FeliDidIt ? pathF : pathP;
-        double abhebung = double.Parse(Abhebebox_tb.Text);
-        if(abhebung > bufferAccount.Money)
+        var abhebung = double.Parse(Abhebebox_tbx.Text);
+        if(abhebung > SelectedAccount.Money)
             {
             MessageBox.Show($"Soviel ist nicht vorhanden!\n" +
-                $"{bufferAccount.Name} hat\n" +
-                $"{bufferAccount.Money.ToString("0.00")} Euro\n" +
-                $"auf dem Konto.");
-            Abhebebox_tb.Text = bufferAccount.Money.ToString("0.00");
+                            $"{SelectedAccount.Name} hat\n" +
+                            $"{SelectedAccount.Money.ToString("0.00")} Euro\n" +
+                            $"auf dem Konto.");
+            Abhebebox_tbx.Text = SelectedAccount.Money.ToString("0.00");
             }
         else
             {
-            bufferAccount.Money -= abhebung;
-            MessageBox.Show($"{abhebung.ToString("0.00")} Euro erfolgreich abgebucht!\n" +
-                $"Es verbleiben {bufferAccount.Money.ToString("0.00")} Euro\n" +
-                $" auf dem Konto von {bufferAccount.Name}.");
-            bufferAccount.Date = Today;
-            File.WriteAllText(path, JsonConvert.SerializeObject(bufferAccount));
+            SelectedAccount.Money -= abhebung;
+            MessageBox.Show($"{abhebung.ToString("0.00")}" +
+                            $" Euro erfolgreich abgebucht!\n" +
+                            $"Es verbleiben {SelectedAccount.Money.ToString("0.00")} Euro\n" +
+                            $" auf dem Konto von {SelectedAccount.Name}.");
+            SelectedAccount.Date = Today;
             }
         }
-    }
-#endregion
 
+    #endregion Abbuchungsvorgang
 
-public class BankAccount
-    {
-    public string Name { get; set; }
-    public DateTime Date { get; set; }
-    public double Money { get; set; }
+    #region Clean off before close
 
-    public BankAccount() { }
-    public BankAccount(string name, DateTime date, double money)
+    //Safe on close statement
+    private void SafeBankAccounts()
         {
-        this.Name = name;
-        this.Date = date;
-        this.Money = money;
+        File.WriteAllText(path, JsonConvert.SerializeObject(BankAccounts));
         }
+
+    #region Safe on close statement alternative
+
+    /*  private void SaveDataToCSV()
+      {
+          // Assuming you have a list of BankAccounts called "BankAccounts"
+          string filePath = "data.csv"; // Specify the file path for your CSV file
+
+          using(StreamWriter writer = new StreamWriter(filePath))
+          {
+              // Write the header
+              writer.WriteLine("Account Number,Name,Balance"); // Adjust the header based on your BankAccount properties
+
+              // Write each BankAccount data
+              foreach(BankAccount account in BankAccounts)
+              {
+                  string line = $"{account.AccountNumber},{account.Name},{account.Balance}"; // Adjust the properties based on your BankAccount class
+                  writer.WriteLine(line);
+              }
+          }
+      }*/
+
+    #endregion Safe on close statement alternative
+
+    private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+        // Perform your save operation here
+        // SaveDataToCSV();
+        }
+
+    #endregion
     }
-
-
-
-/* schau dir das nochmal in ruhe an
-/// <summary>
-/// Writes the given object instance to a binary file.
-/// <para>Object type (and all child types) must be decorated with the [Serializable] attribute.</para>
-/// <para>To prevent a variable from being serialized, decorate it with the [NonSerialized] attribute;
-/// cannot be applied to properties.</para>
-/// </summary>
-/// <typeparam name="T">The type of object being written to the binary file.</typeparam>
-/// <param name="filePath">The file path to write the object instance to.</param>
-/// <param name="objectToWrite">The object instance to write to the binary file.</param>
-/// <param name="append">If false the file will be overwritten if it already exists.
-/// If true the contents will be appended to the file.</param>
-public static void WriteToBinaryFile<T>(string filePath, T objectToWrite, bool append = false)
-{
-using(Stream stream = File.Open(filePath, append ? FileMode.Append : FileMode.Create))
-    {
-    var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
-    binaryFormatter.Serialize(stream, objectToWrite);
-    }
-}
-
-/// <summary>
-/// Reads an object instance from a binary file.
-/// </summary>
-/// <typeparam name="T">The type of object to read from the binary file.</typeparam>
-/// <param name="filePath">The file path to read the object instance from.</param>
-/// <returns>Returns a new instance of the object read from the binary file.</returns>
-public static BankAccount ReadFromBinaryFile<BankAccount>(string filePath)
-{
-using(Stream stream = File.Open(filePath, FileMode.Open))
-    {
-    var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
-    return (BankAccount)binaryFormatter.Deserialize(stream);
-    }
-}
-}
-
-*/
-
