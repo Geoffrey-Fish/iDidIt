b70f500b701e2d64a6b475acf8d064a6ef190066
Geoffrey-Fish <earl@geoffrey.fish>
2023-06-20 17:59:37 +0200

    Holy guacamole ! super productive work today. And no, im not a designer in the least...

--- CarParking/MainWindow.xaml.cs
+++ CarParking/MainWindow.xaml.cs
@@ -1,13 +1,65 @@
-﻿namespace CarParking
+﻿using System.Windows;
+
+namespace CarParking
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+//stop: STOP: todo: here we go tomorrow, good job today
+        private void CreatePhouse_btn_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void LoadLastPh_btn_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Car_btn_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void MoCy_btn_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Truck_btn_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void ParkIn_btn_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void DriveOut_btn_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void SearchLicence_btn_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void SearchType_btn_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void SearchLot_btn_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
